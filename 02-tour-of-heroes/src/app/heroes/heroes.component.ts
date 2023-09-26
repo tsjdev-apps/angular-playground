3c19914557111e6cb06762a78c8fdc91b3ebef40
Sebastian Jensen <apps@tsjdev-apps.de>
2023-09-26 07:47:14 +0200

    Get data from a server

--- 02-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ 02-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,7 +9,7 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {  
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(
@@ -23,5 +23,20 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 }
