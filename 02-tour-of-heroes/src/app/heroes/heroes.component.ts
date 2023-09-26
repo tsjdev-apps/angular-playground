49eab5534b2e0f5d3c16c1c34184bfd99dbe485c
Sebastian Jensen <apps@tsjdev-apps.de>
2023-09-26 07:16:14 +0200

    Display a list

--- 02-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ 02-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
